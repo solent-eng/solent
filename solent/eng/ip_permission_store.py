9febf6ef20ceaefc38a3a9a40a95241bbcfbe747
Craig Turner <cratuki@gmail.com>
2016-11-30 12:25:10 +0000

    Did some experimental work: a different way of protocol work. Have found that to be a dead end. This ip permission store was developing in that batch of work, and will have value in the next approach I try (build proto server by using a nearcast approach).

---
+++ solent/eng/ip_permission_store.py
@@ -0,0 +1,47 @@
+#
+# ip_permission_store
+#
+# // overview
+# Simple mechanism for testing whether an IP is permissioned or not. At the
+# time of writing there is no support for pattern matching. But it's easy
+# to imagine now adding this without breaking the existing interface.
+#
+# // license
+# Copyright 2016, Free Software Foundation.
+#
+# This file is part of Solent.
+#
+# Solent is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Solent is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Solent. If not, see <http://www.gnu.org/licenses/>.
+
+class IpPermissionStore:
+    def __init__(self):
+        self.b_accept_any_ip = False
+        self.permissioned_ips = []
+    def accept_any_ip(self, b=True):
+        self.b_accept_any_ip = b
+    def is_ok(self, ip):
+        if self.b_accept_any_ip:
+            return True
+        if ip in self.permissioned_ips:
+            return True
+        return False
+    def add_ip(self, ip):
+        if ip == '0.0.0.0':
+            raise Exception("Please do not do this.")
+        self.permissioned_ips.append(ip)
+
+def ip_permission_store_new():
+    ob = IpPermissionStore()
+    return ob
+
---
+++ testing/eng/ip_permission_store.py
@@ -0,0 +1,74 @@
+#
+# ip_permission_store (testing)
+#
+# // license
+# Copyright 2016, Free Software Foundation.
+#
+# This file is part of Solent.
+#
+# Solent is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Solent is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Solent. If not, see <http://www.gnu.org/licenses/>.
+
+from testing import run_tests
+from testing import test
+from testing.eng import engine_fake
+from testing.util import clock_fake
+
+from solent.eng import ip_permission_store_new
+
+import sys
+
+@test
+def should_accept_and_reject_ips():
+    test_ip = '127.0.0.1'
+    second_ip = '203.15.93.2'
+    #
+    ip_permission_store = ip_permission_store_new()
+    ip_permission_store.add_ip(
+        ip=test_ip)
+    #
+    assert True == ip_permission_store.is_ok(
+        ip=test_ip)
+    assert False == ip_permission_store.is_ok(
+        ip=second_ip)
+    #
+    ip_permission_store.add_ip(
+        ip=second_ip)
+    assert True == ip_permission_store.is_ok(
+        ip=test_ip)
+    assert True == ip_permission_store.is_ok(
+        ip=second_ip)
+    #
+    return True
+
+@test
+def should_allow_all_ips_to_be_accepted():
+    test_ip = '127.0.0.1'
+    second_ip = '203.15.93.2'
+    #
+    ip_permission_store = ip_permission_store_new()
+    #
+    assert False == ip_permission_store.is_ok(
+        ip=test_ip)
+    #
+    ip_permission_store.accept_any_ip()
+    #
+    assert True == ip_permission_store.is_ok(
+        ip=test_ip)
+    #
+    return True
+
+if __name__ == '__main__':
+    run_tests(
+        unders_file=sys.modules['__main__'].__file__)
+
