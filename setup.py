8f4e2b0a0ac71ef3cbe66e6b42a643d01210ee04
Craig Turner <cratuki@users.noreply.github.com>
2016-12-05 16:36:26 +0000

    fixing problems found from release to pypi

--- setup.py
+++ setup.py
@@ -19,27 +19,48 @@
 # You should have received a copy of the GNU General Public License along with
 # Solent. If not, see <http://www.gnu.org/licenses/>.
 
-import sys
-if sys.version_info.major < 3:
-    print('''
-        I'm only for python 3. There is a version of the eng networking
-        library maintained for python 2, but it's not offered through pip at
-        the moment. Check it out at github.com/cratuki/eng. If you get into
-        trouble, feel free to contact me using (github username) at (google
-        mail service) dot com.
-    ''')
-    sys.exit(1)
-
 from setuptools import setup
+from setuptools import find_packages
 
-setup(
-  name = 'solent',
-  packages = ['solent'],
-  version = '0.15',
-  description = 'Event-driven concurrency engine',
-  url = 'https://github.com/cratuki/solent',
-  download_url = 'https://github.com/cratuki/solent/tarball/0.15',
-  keywords = ['solent', 'eng', 'term', 'networking', 'roguelikes', 'async'],
-  classifiers = [],
-)
+import os
+import sys
 
+
+def check_python_version():
+    if sys.version_info.major < 3:
+        print('''
+          I'm only for python 3. There is a version of the eng networking
+          library maintained for python 2, but it's not offered through pip
+          at the moment. Check it out at github.com/cratuki/eng. If you get
+          into trouble, feel free to contact me using (github username) at
+          (google mail service) dot com.
+        ''')
+        sys.exit(1)
+
+
+def main():
+    check_python_version()
+
+    solent_packages = sorted([pname for pname in find_packages() if (
+        pname == 'solent' or pname.startswith('solent.'))])
+
+    setup(
+        name='solent',
+        packages=solent_packages,
+        version='0.15',
+        description='Event-driven concurrency engine',
+        url='https://github.com/cratuki/solent',
+        download_url='https://github.com/cratuki/solent/tarball/0.15',
+        keywords=[
+            'solent',
+            'eng',
+            'term',
+            'networking',
+            'roguelikes',
+            'async'],
+        classifiers=[],
+        include_package_data=True,
+    )
+
+if __name__ == '__main__':
+    main()
