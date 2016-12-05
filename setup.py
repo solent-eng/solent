c703d5a9449dae0475cc06626a706bafc1d64be5
Craig Turner <cratuki@users.noreply.github.com>
2016-12-05 16:54:19 +0000

    Trying to get TTF file included in pypi release.

--- setup.py
+++ setup.py
@@ -47,10 +47,10 @@ def main():
     setup(
         name='solent',
         packages=solent_packages,
-        version='0.15',
+        version='0.17',
         description='Event-driven concurrency engine',
         url='https://github.com/cratuki/solent',
-        download_url='https://github.com/cratuki/solent/tarball/0.15',
+        download_url='https://github.com/cratuki/solent/tarball/0.17',
         keywords=[
             'solent',
             'eng',
