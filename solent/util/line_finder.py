be7f3ffa19e51ce0bfe9e2489c8a46ea5aed218a
Craig Turner <cratuki@users.noreply.github.com>
2016-12-11 21:09:26 +0000

    Added license text for line_finder

--- solent/util/line_finder.py
+++ solent/util/line_finder.py
@@ -1,3 +1,32 @@
+#!/usr/bin/env python3
+#
+# line finder
+#
+# // overview
+# Useful when you are looking to identify a line of text as it comes. This
+# class can be run against streams, and issues a callback each time it finds
+# a line.
+#
+# // license
+# Copyright 2016, Free Software Foundation.
+#
+# This file is part of Solent.
+#
+# Solent is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Solent is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Solent. If not, see <http://www.gnu.org/licenses/>.
+
+from solent.log import log
+
 import types
 
 class LineFinder:
