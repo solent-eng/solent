68e2e685b88934c52b3bdadc89ce9f9b4838a7af
Craig Turner <cratuki@users.noreply.github.com>
2017-01-06 16:51:18 +0000

    Have made a start on roguebox. I'm going stale, so committing this and refocussing on some other work.

---
+++ solent/draft/roguebox.py
@@ -0,0 +1,375 @@
+#!/usr/bin/env python3
+#
+# roguebox
+#
+# // overview
+# Nearcast that is designed to contain a roguelike game.
+#
+# // license
+# Copyright 2016, Free Software Foundation.
+#
+# This file is part of Solent.
+#
+# Solent is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Solent is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Solent. If not, see <http://www.gnu.org/licenses/>.
+
+from solent.console import cgrid_new
+from solent.console import e_colpair
+from solent.console import key
+from solent.rogue.swamp_monster import spin_swamp_monster_new
+from solent.eng import engine_new
+from solent.eng import nearcast_schema_new
+from solent.exceptions import SolentQuitException
+from solent.log import init_logging
+from solent.log import log
+from solent.term import spin_term_new
+from solent.menu import spin_menu_new
+from solent.util import uniq
+
+from collections import deque
+import os
+import random
+import sys
+import time
+import traceback
+
+MTU = 1500
+
+# Containment consists of a menu system, a terminal, and cogs that
+# encapsulate games.
+I_CONTAINMENT_NEARCAST_SCHEMA = '''
+    i message h
+        i field h
+
+    message init
+        field height
+        field width
+
+    message quit
+
+    message keystroke
+        field keycode
+
+    message game_focus
+    message game_new
+    message game_input
+        field keycode
+
+    message term_clear
+    message term_write
+        field drop
+        field rest
+        field s
+        field cpair
+
+    message menu_focus
+    message menu_title
+        field text
+    message menu_item
+        field menu_keycode
+        field text
+    message menu_select
+        field menu_keycode
+'''
+
+MENU_KEYCODE_NEW_GAME = key('n')
+MENU_KEYCODE_CONTINUE = key('c')
+MENU_KEYCODE_QUIT = key('q')
+
+def t100():
+    return time.time() * 100
+
+class PinContainmentMode:
+    '''
+    Tracks whether we are in the menu or not.
+    '''
+    def __init__(self):
+        self.b_in_menu = True
+    #
+    def on_menu_focus(self):
+        self.b_in_menu = True
+    def on_game_focus(self):
+        self.b_in_menu = False
+    #
+    def is_focus_on_menu(self):
+        return self.b_in_menu
+    def is_focus_on_game(self):
+        return not self.b_in_menu
+
+class CogInterpreter:
+    '''
+    Coordinates high-level concepts such as whether we are in a menu or in the
+    game.
+    '''
+    def __init__(self, cog_h, engine, orb):
+        self.cog_h = cog_h
+        self.engine = engine
+        self.orb = orb
+        #
+        self.pin_containment_mode = orb.init_pin(PinContainmentMode)
+    def on_quit(self):
+        raise SolentQuitException('Quit message on stream')
+    def on_keystroke(self, keycode):
+        if self.pin_containment_mode.is_focus_on_menu():
+            if keycode == key('tab'):
+                self.b_in_menu = False
+                self.nearcast.game_focus()
+            else:
+                self.nearcast.menu_select(
+                    menu_keycode=keycode)
+        else:
+            if keycode == key('tab'):
+                self.b_in_menu = True
+                self.nearcast.menu_focus()
+            else:
+                self.nearcast.game_input(
+                    keycode=keycode)
+
+class CogTerm:
+    def __init__(self, cog_h, engine, orb):
+        self.cog_h = cog_h
+        self.engine = engine
+        self.orb = orb
+        #
+        self.spin_term = spin_term_new(
+            cb_keycode=self.term_on_keycode,
+            cb_select=self.term_on_select)
+    def close(self):
+        self.spin_term.close()
+    def at_turn(self, activity):
+        self.spin_term.at_turn(
+            activity=activity)
+    def on_term_clear(self):
+        self.spin_term.clear()
+    def on_term_write(self, drop, rest, s, cpair):
+        self.spin_term.write(
+            drop=drop,
+            rest=rest,
+            s=s,
+            cpair=cpair)
+    #
+    def term_on_keycode(self, keycode):
+        self.nearcast.keystroke(
+            keycode=keycode)
+    def term_on_select(self, drop, rest):
+        # user makes a selection
+        log('xxx term_on_select drop %s rest %s'%(drop, rest))
+    #
+    def on_init(self, height, width):
+        self.spin_term.open_console(
+            width=width,
+            height=height)
+
+class CogMenu:
+    def __init__(self, cog_h, engine, orb):
+        self.cog_h = cog_h
+        self.engine = engine
+        self.orb = orb
+        #
+        self.spin_menu = None
+    def close(self):
+        pass
+    def on_init(self, height, width):
+        self.spin_menu = spin_menu_new(
+            height=height,
+            width=width,
+            cb_display_clear=self.menu_display_clear,
+            cb_display_write=self.menu_display_write)
+        self.nearcast.menu_title(
+            text=__name__)
+        self.nearcast.menu_item(
+            menu_keycode=MENU_KEYCODE_NEW_GAME,
+            text='new game')
+        self.nearcast.menu_item(
+            menu_keycode=MENU_KEYCODE_QUIT,
+            text='quit')
+        self.nearcast.menu_focus()
+    def on_menu_title(self, text):
+        self.spin_menu.set_title(
+            text=text)
+    def on_menu_item(self, menu_keycode, text):
+        self.spin_menu.add_menu_item(
+            menu_keycode=menu_keycode,
+            text=text,
+            cb_select=lambda: self.menu_select(
+                menu_keycode=keycode))
+    def on_menu_focus(self):
+        self.spin_menu.render_menu()
+    def on_menu_select(self, menu_keycode):
+        d = { MENU_KEYCODE_NEW_GAME: self._mi_new_game
+            , MENU_KEYCODE_CONTINUE: self._mi_continue
+            , MENU_KEYCODE_QUIT: self._mi_quit
+            }
+        if menu_keycode not in d:
+            return
+        fn = d[menu_keycode]
+        fn()
+    def on_game_new(self):
+        if not self.spin_menu.has_menu_keycode(MENU_KEYCODE_CONTINUE):
+            self.nearcast.menu_item(
+                menu_keycode=MENU_KEYCODE_CONTINUE,
+                text='continue')
+    #
+    def menu_select(self, menu_keycode):
+        self.nearcast.menu_select(
+            menu_keycode=menu_keycode)
+    def menu_display_clear(self):
+        self.nearcast.term_clear()
+    def menu_display_write(self, drop, rest, s):
+        self.nearcast.term_write(
+            drop=drop,
+            rest=rest,
+            s=s,
+            cpair=e_colpair.blue_t)
+    #
+    def _mi_new_game(self):
+        self.nearcast.game_new()
+        self.nearcast.game_focus()
+    def _mi_continue(self):
+        self.nearcast.game_focus()
+    def _mi_quit(self):
+        raise SolentQuitException()
+
+ROGUEBOX_GAME_HEIGHT = 23
+ROGUEBOX_GAME_WIDTH = 23
+
+class CogRoguebox:
+    '''
+    Contains a roguelike game, and offers controls. The roguelike itself
+    is contained to a 23x23 box in the top-left sector.
+    '''
+    def __init__(self, cog_h, engine, orb):
+        self.cog_h = cog_h
+        self.engine = engine
+        self.orb = orb
+        #
+        self.pin_containment_mode = orb.init_pin(PinContainmentMode)
+        self.height = None
+        self.width = None
+        self.spin_roguelike = None
+    def close(self):
+        pass
+    def at_turn(self, activity):
+        if self.spin_roguelike == None:
+            return
+        if not self.pin_containment_mode.is_focus_on_game():
+            return
+    #
+    def on_init(self, height, width):
+        self.height = height
+        self.width = width
+    def on_game_new(self):
+        self.spin_roguelike = spin_swamp_monster_new(
+            engine=self.engine,
+            height=ROGUEBOX_GAME_HEIGHT,
+            width=ROGUEBOX_GAME_WIDTH,
+            cb_cls=self.rl_cls,
+            cb_put=self.rl_put,
+            cb_box=self.rl_box)
+    def on_game_input(self, keycode):
+        if keycode in (key('q'), key('y'), key('n7')):
+            self.spin_roguelike.input_nw()
+        elif keycode in (key('w'), key('k'), key('n8')):
+            self.spin_roguelike.input_nn()
+        elif keycode in (key('e'), key('u'), key('n9')):
+            self.spin_roguelike.input_ne()
+        elif keycode in (key('z'), key('b'), key('n1')):
+            self.spin_roguelike.input_sw()
+        elif keycode in (key('x'), key('j'), key('n2')):
+            self.spin_roguelike.input_ss()
+        elif keycode in (key('c'), key('n'), key('n3')):
+            self.spin_roguelike.input_se()
+        elif keycode in (key('a'), key('h'), key('n4')):
+            self.spin_roguelike.input_ww()
+        elif keycode in (key('d'), key('l'), key('n6')):
+            self.spin_roguelike.input_ee()
+        elif keycode in (key('s'), key('newline'), key('space'), key('n5')):
+            self.spin_roguelike.input_bump()
+    def on_game_focus(self):
+        if self.spin_roguelike == None:
+            self.nearcast.menu_focus()
+            return
+        self.spin_roguelike.full_refresh()
+    #
+    def rl_cls(self):
+        self.nearcast.term_clear()
+    def rl_put(self, drop, rest, s, cpair):
+        self.nearcast.term_write(
+            drop=drop,
+            rest=rest,
+            s=s,
+            cpair=cpair)
+    def rl_box(self, message):
+        self.nearcast.term_clear()
+        lines = message.split('\n')
+        #
+        height = len(lines)
+        width = max([len(l) for l in lines])
+        drop_offset = int( (self.height - height) / 2 )
+        rest_offset = int( (self.width - width) / 2 )
+        #
+        for idx, line in enumerate(lines):
+            self.nearcast.term_write(
+                drop=drop_offset+idx,
+                rest=rest_offset,
+                s=line,
+                cpair=e_colpair.white_t)
+
+class CogBridge:
+    def __init__(self, cog_h, orb, engine):
+        self.cog_h = cog_h
+        self.orb = orb
+        self.engine = engine
+    def nc_init(self, height, width):
+        self.nearcast.init(
+            height=height,
+            width=width)
+
+def main():
+    init_logging()
+    #
+    engine = None
+    try:
+        engine = engine_new(
+            mtu=MTU)
+        engine.set_default_timeout(0.04)
+        engine.debug_eloop_on() # xxx
+        #
+        nearcast_schema = nearcast_schema_new(
+            i_nearcast=I_CONTAINMENT_NEARCAST_SCHEMA)
+        orb = engine.init_orb(
+            orb_h=__name__,
+            nearcast_schema=nearcast_schema)
+        orb.add_log_snoop()
+        orb.init_cog(CogInterpreter)
+        orb.init_cog(CogTerm)
+        orb.init_cog(CogMenu)
+        orb.init_cog(CogRoguebox)
+        #
+        bridge = orb.init_cog(CogBridge)
+        bridge.nc_init(
+            height=24,
+            width=80)
+        #
+        engine.event_loop()
+    except SolentQuitException:
+        pass
+    except:
+        traceback.print_exc()
+    finally:
+        if engine != None:
+            engine.close()
+
+if __name__ == '__main__':
+    main()
+
---
+++ solent/rogue/swamp_monster/spin_swamp_monster.py
@@ -0,0 +1,410 @@
+#
+# swamp monster
+#
+# // overview
+# Adaptation of a previous 7d-roguelike to solent. Being designed within
+# CogRoguebox (solent.draft.roguebox).
+#
+# // license
+# Copyright 2016, Free Software Foundation.
+#
+# This file is part of Solent.
+#
+# Solent is free software: you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# Solent is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Solent. If not, see <http://www.gnu.org/licenses/>.
+
+from .chart import chart_new
+from .swamp import swamp_new
+
+from solent.console import e_colpair
+from solent.eng import nearcast_schema_new
+from solent.log import log
+from solent.util import uniq
+
+import random
+
+I_NEARCAST = '''
+    i message h
+        i field h
+
+    message init
+        field height
+        field width
+
+    message box_display
+        field message
+
+    message full_refresh
+
+    message sigil
+        field h
+
+    message tile
+        field sigil_h
+        field drop
+        field rest
+
+    message meep
+        field h
+        field rest
+        field drop
+        field c
+        field cpair
+
+    message input_nw
+    message input_nn
+    message input_ne
+    message input_sw
+    message input_ss
+    message input_se
+    message input_ww
+    message input_ee
+    message input_bump
+
+    message move_player
+        field h
+        field drop
+        field rest
+
+    message move_monster
+        field h
+        field drop
+        field rest
+
+    message end_of_round
+
+    message display_clear
+    message display_put
+        field drop
+        field rest
+        field s
+        field cpair
+'''
+
+DM_NAV = uniq()
+DM_BOX = uniq()
+
+class PinDisplayMode:
+    def __init__(self):
+        self.mode = None
+    #
+    def on_init(self, height, width):
+        self.mode = DM_NAV
+    #
+    def is_nav_mode(self):
+        return self.mode == DM_NAV
+    def is_box_mode(self):
+        return self.mode == DM_BOX
+
+SIGIL_VOID = ' '
+SIGIL_WATER = '~'
+SIGIL_LAND = '.'
+SIGIL_MANGROVE = '%'
+SIGIL_MONSTER = 'Y'
+SIGIL_PLAYER = '@'
+
+class PinLayout:
+    def __init__(self):
+        self.player_spot = None
+        self.monster_spots = []
+        #
+        self.chart_ground = None    # water, land
+        self.chart_mobile = None    # mangrove, monster, player
+        self.chart_composite = None
+        self.is_ready = False
+    def on_init(self, height, width):
+        self.chart_ground = chart_new(
+            height=height,
+            width=width)
+        self.chart_mobile = chart_new(
+            height=height,
+            width=width)
+        self.chart_composite = chart_new(
+            height=height,
+            width=width)
+    def on_terrain_water(self, drop, rest):
+        self.chart_ground.put(
+            spot=(drop, rest),
+            c=SIGIL_WATER)
+    def on_terrain_land(self, drop, rest):
+        self.chart_ground.put(
+            spot=(drop, rest),
+            c=SIGIL_LAND)
+    def on_terrain_mangrove(self, drop, rest):
+        self.chart_mobile.put(
+            spot=(drop, rest),
+            c=SIGIL_MANGROVE)
+    def on_terrain_done(self):
+        self.is_ready = True
+    #
+    def get_chart(self):
+        'Returns [ (spot, c) ]'
+        for (spot, c) in self.chart_ground.get():
+            if c == SIGIL_VOID:
+                continue
+            self.chart_composite.put(
+                spot=spot,
+                c=c)
+        for (spot, c) in self.chart_mobile.get():
+            if c == SIGIL_VOID:
+                continue
+            self.chart_composite.put(
+                spot=spot,
+                c=c)
+        return self.chart_composite.get()
+
+class CogLadyOfTheLake:
+    '''
+    The lady is responsible for:
+    * Setting up the swamp
+    * Controlling the monsters inside it
+    '''
+    def __init__(self, cog_h, orb, engine):
+        self.cog_h = cog_h
+        self.orb = orb
+        self.engine = engine
+        #
+        self.pin_layout = orb.init_pin(PinLayout)
+        self.monsters = []
+    def on_order_monster_turn(self):
+        if random.random() > 0.8:
+            self._create_monster()
+    #
+    def _create_monster(self):
+        log('xxx create monster')
+
+class CogView:
+    '''
+    The view is responsible for detecting changes to the board, and updating
+    the world as to the changes after each of the lady's move.
+
+    Separately, a full refresh is needed at the beginning of the game than
+    periodically after that. The view does this also.
+    '''
+    def __init__(self, cog_h, orb, engine):
+        self.cog_h = cog_h
+        self.orb = orb
+        self.engine = engine
+        #
+        self.chart = None
+        self.board = None
+        self.pin_layout = orb.init_pin(PinLayout)
+    def on_init(self, height, width):
+        swamp = swamp_new(
+            height=height,
+            width=width)
+        chart = chart_new(
+            height=height,
+            width=width)
+        swamp.populate_chart(
+            chart=chart)
+        for (spot, c) in chart.get():
+            (drop, rest) = spot
+            if c == '~':
+                self.nearcast.terrain_water(
+                    drop=drop,
+                    rest=rest)
+            elif c == '.':
+                self.nearcast.terrain_land(
+                    drop=drop,
+                    rest=rest)
+            elif c == '%':
+                self.nearcast.terrain_mangrove(
+                    drop=drop,
+                    rest=rest)
+            else:
+                raise Exception("Unrecognised terrain [%s]"%c)
+        self.nearcast.terrain_done()
+    def on_terrain_done(self):
+        self.nearcast.full_refresh()
+    def on_full_refresh(self):
+        if not self.pin_layout.is_ready:
+            return
+        self.nearcast.display_clear()
+        for (spot, c) in self.pin_layout.get_chart():
+            (drop, rest) = spot
+            log('here %s %s'%(str(drop), str(rest)))
+            if c == SIGIL_WATER:
+                self.nearcast.display_put(
+                    drop=drop,
+                    rest=rest,
+                    s=c,
+                    cpair=e_colpair.blue_t)
+            elif c == SIGIL_LAND:
+                self.nearcast.display_put(
+                    drop=drop,
+                    rest=rest,
+                    s=c,
+                    cpair=e_colpair.white_t)
+            elif c == SIGIL_PLAYER:
+                self.nearcast.display_put(
+                    drop=drop,
+                    rest=rest,
+                    s=c,
+                    cpair=e_colpair.yellow_t)
+            elif c == SIGIL_MANGROVE:
+                self.nearcast.display_put(
+                    drop=drop,
+                    rest=rest,
+                    s=c,
+                    cpair=e_colpair.green_t)
+            elif c == SIGIL_MONSTER:
+                self.nearcast.display_put(
+                    drop=drop,
+                    rest=rest,
+                    s=c,
+                    cpair=e_colpair.red_t)
+            else:
+                self.nearcast.display_put(
+                    drop=drop,
+                    rest=rest,
+                    s='x',
+                    cpair=e_colpair.red_t)
+
+class CogPlayer:
+    def __init__(self, cog_h, orb, engine):
+        self.cog_h = cog_h
+        self.orb = orb
+        self.engine = engine
+        #
+        self.pin_display_mode = orb.init_pin(PinDisplayMode)
+    def on_input_nw(self):
+        self.nearcast.order_monster_turn()
+    def on_input_nn(self):
+        self.nearcast.order_monster_turn()
+    def on_input_ne(self):
+        self.nearcast.order_monster_turn()
+    def on_input_sw(self):
+        self.nearcast.order_monster_turn()
+    def on_input_ss(self):
+        self.nearcast.order_monster_turn()
+    def on_input_se(self):
+        self.nearcast.order_monster_turn()
+    def on_input_ww(self):
+        self.nearcast.order_monster_turn()
+    def on_input_ee(self):
+        self.nearcast.order_monster_turn()
+
+class CogBridge:
+    def __init__(self, cog_h, orb, engine):
+        self.cog_h = cog_h
+        self.orb = orb
+        self.engine = engine
+    def set_callbacks(self, cb_cls, cb_put, cb_box):
+        self.cb_cls = cb_cls
+        self.cb_put = cb_put
+        self.cb_box = cb_box
+    def nc_init(self, height, width):
+        self.nearcast.init(
+            height=height,
+            width=width)
+    def nc_input_nw(self):
+        self.nearcast.input_nw()
+    def nc_input_nn(self):
+        self.nearcast.input_nn()
+    def nc_input_ne(self):
+        self.nearcast.input_ne()
+    def nc_input_sw(self):
+        self.nearcast.input_sw()
+    def nc_input_ss(self):
+        self.nearcast.input_ss()
+    def nc_input_ww(self):
+        self.nearcast.input_ww()
+    def nc_input_ee(self):
+        self.nearcast.input_ee()
+    def nc_input_se(self):
+        self.nearcast.input_se()
+    def nc_input_bump(self):
+        self.nearcast.input_bump()
+    def nc_full_refresh(self):
+        self.nearcast.full_refresh()
+    #
+    def on_box_display(self, message):
+        self.cb_box(
+            message=message)
+    def on_display_clear(self):
+        self.cb_cls()
+    def on_display_put(self, drop, rest, s, cpair):
+        self.cb_put(
+            drop=drop,
+            rest=rest,
+            s=s,
+            cpair=cpair)
+
+class SpinSwampMonster:
+    def __init__(self, engine, height, width, cb_cls, cb_put, cb_box):
+        self.engine = engine
+        self.height = height
+        self.width = width
+        self.cb_cls = cb_cls
+        self.cb_put = cb_put
+        self.cb_box = cb_box
+        #
+        self.nearcast = nearcast_schema_new(
+            i_nearcast=I_NEARCAST)
+        self.orb = self.engine.init_orb(
+            orb_h='swamp_monster_orb',
+            nearcast_schema=self.nearcast)
+        self.orb.add_log_snoop()
+        self.orb.init_cog(CogLadyOfTheLake)
+        self.orb.init_cog(CogView)
+        self.orb.init_cog(CogPlayer)
+        #
+        self.bridge = self.orb.init_cog(CogBridge)
+        self.bridge.set_callbacks(
+            cb_cls=cb_cls,
+            cb_put=cb_put,
+            cb_box=cb_box)
+        self.bridge.nc_init(
+            height=self.height,
+            width=self.width)
+    def input_nw(self):
+        self.bridge.nc_input_nw()
+    def input_nn(self):
+        self.bridge.nc_input_nn()
+    def input_ne(self):
+        self.bridge.nc_input_ne()
+    def input_sw(self):
+        self.bridge.nc_input_sw()
+    def input_ss(self):
+        self.bridge.nc_input_ss()
+    def input_se(self):
+        self.bridge.nc_input_se()
+    def input_ww(self):
+        self.bridge.nc_input_ww()
+    def input_ee(self):
+        self.bridge.nc_input_ee()
+    def input_bump(self):
+        self.bridge.nc_input_bump()
+    def full_refresh(self):
+        self.bridge.nc_full_refresh()
+
+def spin_swamp_monster_new(engine, height, width, cb_cls, cb_put, cb_box):
+    '''
+    cb_cls()
+        # clear screen
+
+    cb_put(drop, rest, s, cpair)
+
+    cb_box(s)
+        # message box
+    '''
+    ob = SpinSwampMonster(
+        engine=engine,
+        height=height,
+        width=width,
+        cb_cls=cb_cls,
+        cb_put=cb_put,
+        cb_box=cb_box)
+    return ob
+
