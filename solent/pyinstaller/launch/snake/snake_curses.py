ff5fd08fa858544f874a520a0418f17ef909b1d2
Craig Turner <cratuki@users.noreply.github.com>
2017-01-12 15:50:11 +0000

    Moved snake code from draft to release.

--- solent/pyinstaller/launch/snake/snake_curses.py
+++ solent/pyinstaller/launch/snake/snake_curses.py
@@ -20,7 +20,7 @@
 # You should have received a copy of the GNU General Public License along with
 # Solent. If not, see <http://www.gnu.org/licenses/>.
 
-from solent.draft.snake import game
+from solent.release.snake import game
 
 from solent.log import log
 
