65bc2e85c574ca6e23433204cf4d21705e66a401
Craig Turner <cratuki@users.noreply.github.com>
2017-10-07 14:19:16 +0100

    issue #94, create convenient launch script for Windows-based development (#95)

---
+++ windows-app.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+LAUNCH = "run_tests"
+#LAUNCH = "solent.demo.snake"
+
+def main():
+	venv_python = os.path.join(BASE_DIR, 'venv', 'Scripts', 'python.exe')
+	if not os.path.exists(venv_python):
+		raise Exception("Assumes venv. (Create venv under the base dir)")
+	#
+	args = [venv_python, '-B', '-m', LAUNCH]
+	print(args)
+	subprocess.Popen(
+		args=args,
+		cwd=BASE_DIR)
+
+if __name__ == '__main__':
+	main()
